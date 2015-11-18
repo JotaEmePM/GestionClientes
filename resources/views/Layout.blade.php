c0037e57eab5c4a3e093b4cfff09e7772c70cf5a
José Miguel Pérez Méndez <perezmjosem@gmail.com>
2015-11-18 14:53:23 -0300

    Instalado Inspinia

---
+++ resources/views/Layout.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Gestion de clientes</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css">
+
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="">
+	<div id="wrapper">
+		<div class="navbar-default navbar-static-side" role="navigation">
+			<div class="sidebar-collapse">
+				<ul class="nav metismenu" id="side-menu">
+					<li class="nav-header">
+						<div class="dropdown profile-element">
+							<span>
+								<img class="img-circle" alt="image" src="img/profile_small.jpg" />
+							</span>
+							<a class="dropdown-toggle" href="#" data-toggle="dropdown">
+								<span class="clear">
+									<span class="block m-t-xs">
+										<strong class="font-bold">Usuario 1</strong>
+									</span>
+									<span class="text-muted text-xs block">Administrador <b class="caret"></b></span>
+								</span>
+							</a>
+							<ul class="dropdown-menu animated fadeInRight m-t-xs">
+								<li><a href="#">Perfíl</a></li>
+								<li><a href="#">Configuracion</a></li>
+								<li><a href="#">Desconectar</a></li>
+							</ul>
+						</div>
+						<div class="logo-element">
+							GC
+						</div>
+					</li>
+					<li>
+						<a href="#">
+							i.<fa class="fa-th-large"></fa>
+							<span class="nav-label">Inicio</span>
+							<span class="fa arrow"></span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
